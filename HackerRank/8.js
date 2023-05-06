1e9a05192f1febfb6951a21869d69d88f7030cb3
SUHONG\topic <topic828@Naver.com>
2023-05-06 18:07:11 +0900

    :memo: 문제풀이
    문제풀이

--- HackerRank/8.js
+++ HackerRank/8.js
@@ -2,16 +2,25 @@
 
 function longestChain(words) {
   // Write your code here
-  let answer = 0;
-  for (let i = 0; i < words.length; i++) {
-    for (let j = 0; j < words.length; j++) {
-      if (
-        words[j].includes(words[i]) &&
-        words[j].length - words[i].length > answer
-      ) {
-        answer = words[j].length - words[i].length;
+  words.sort((a, b) => a.length - b.length);
+
+  const chainLengths = new Map();
+
+  for (const word of words) {
+    chainLengths.set(word, 1);
+  }
+
+  let maxLength = 1;
+  for (const word of words) {
+    for (let i = 0; i < word.length; i++) {
+      const reducedWord = word.slice(0, i) + word.slice(i + 1);
+      if (chainLengths.has(reducedWord)) {
+        const chainLength = chainLengths.get(reducedWord) + 1;
+        chainLengths.set(word, Math.max(chainLengths.get(word), chainLength));
+        maxLength = Math.max(maxLength, chainLength);
       }
     }
   }
-  return answer + 1;
+
+  return maxLength;
 }
---
+++ Programmers/lv1/test.js
@@ -0,0 +1,3 @@
+const a = [1, 2];
+const b = a.map((x) => x * 2);
+console.log(b);
