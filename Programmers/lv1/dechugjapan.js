5ec58b1ca7ff136c85eeedf5dca59b9bd889c613
SUHONG\topic <topic828@Naver.com>
2023-05-20 16:30:52 +0900

    알고리즘 대충만든 자판

---
+++ Programmers/lv1/dechugjapan.js
@@ -0,0 +1,40 @@
+// 문제링크 https://school.programmers.co.kr/learn/courses/30/lessons/160586
+
+function solution(keymap, targets) {
+  let answer = [];
+  let smallestKey = {};
+  //키맵을 순찰해서, 각각의 문자에 대한 최소 횟수를 구함.
+  keymap.forEach((eachKey) => {
+    eachKey.split("").forEach((key, idx) => {
+      //존재하지 않으면 그냥 넣고(인덱스+1이 버튼 누르는 횟수)
+      if (smallestKey[key] === undefined) {
+        smallestKey[key] = idx + 1;
+      } else {
+        //존재한다면 비교해서 최소값을 넣기
+        smallestKey[key] = Math.min(smallestKey[key], idx + 1);
+      }
+    });
+  });
+  // console.log(smallestKey);
+  answer = targets.map((target) =>
+    target.split("").reduce((acc, val) => {
+      //없는 값을 찾으면 중간에 undefined가 나와서 결과값이 NaN이 됨
+      return acc + smallestKey[val];
+    }, 0)
+  );
+  // NaN이 있는 경우 다 수정
+  answer = answer.map((element) => {
+    return isNaN(element) ? -1 : element;
+  });
+
+  return answer;
+}
+
+console.log(solution(["ABACD", "BCEFD"], ["ABCD", "AABB"]));
+console.log(solution(["AA"], ["B"]));
+
+// const least = { A: 1 };
+// console.log(least["B"]);
+// //문자도 인덱스로 추출가능
+// let a = "2345";
+// console.log(a[1]);
