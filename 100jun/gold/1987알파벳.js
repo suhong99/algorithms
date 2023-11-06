c5abd7c0348237b5dc01f4af648fb7f5c94b4a20
SUHONG\topic <topic828@Naver.com>
2023-11-06 20:28:32 +0900

    백트래킹

--- 100jun/gold/1987알파벳.js
+++ 100jun/gold/1987알파벳.js
@@ -10,7 +10,15 @@ for (let i = 1; i <= r; i++) arr.push(input[i]);
 
 const dx = [-1, 1, 0, 0];
 const dy = [0, 0, -1, 1];
-let visited = new Set();
+let visited = {};
+for (
+  let charCode = 'a'.charCodeAt(0);
+  charCode <= 'z'.charCodeAt(0);
+  charCode++
+) {
+  const char = String.fromCharCode(charCode);
+  visited[char] = false;
+}
 let maxDepth = 0;
 
 function dfs(depth, x, y) {
@@ -20,13 +28,13 @@ function dfs(depth, x, y) {
     let nx = x + dx[i];
     let ny = y + dy[i];
     if (nx < 0 || nx >= r || ny < 0 || ny >= c) continue;
-    if (visited.has(arr[nx][ny])) continue;
-    visited.add(arr[nx][ny]);
+    if (visited[arr[nx][ny]]) continue;
+    visited[arr[nx][ny]] = true;
     dfs(depth + 1, nx, ny);
-    visited.delete(arr[nx][ny]);
+    visited[arr[nx][ny]] = false;
   }
 }
 
-visited.add(arr[0][0]);
+visited[arr[0][0]] = true;
 dfs(1, 0, 0);
 console.log(maxDepth);
---
+++ 100jun/silver/2529부등호.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync(__dirname + '/input.txt')
+  .toString()
+  .split('\n');
+
+let k = Number(input[0]);
+let arr = input[1].split(' ');
+let visited = new Array(10).fill(false);
+let result = [];
+let current = '';
+let first = '';
+
+function dfs(depth) {
+  if (depth == k + 1) {
+    let check = true;
+    for (let i = 0; i < k; i++) {
+      if (arr[i] === '<') {
+        if (result[i] > result[i + 1]) check = false;
+      } else {
+        if (result[i] < result[i + 1]) check = false;
+      }
+    }
+    if (check) {
+      current = '';
+      for (let x of result) current += x + '';
+      if (first == '') first = current;
+    }
+    return;
+  }
+  for (let i = 0; i < 10; i++) {
+    if (visited[i]) continue;
+    visited[i] = true;
+    result.push(i);
+    dfs(depth + 1);
+    visited[i] = false;
+    result.pop();
+  }
+}
+
+dfs(0);
+console.log(current + '\n' + first);
