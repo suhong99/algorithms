79a011883858ee71743dbd997c26c6a74b96da57
SUHONG\topic <topic828@Naver.com>
2023-12-19 22:36:50 +0900

    every 메서드

--- 100jun/test.js
+++ 100jun/test.js
@@ -3,3 +3,9 @@ const test = 'How Are you?';
 console.log(test.toLowerCase());
 
 console.log(test[0]);
+
+const testArr = [false, false];
+
+const testMethod = (e) => e === false;
+
+console.log(testArr.every(testMethod));
