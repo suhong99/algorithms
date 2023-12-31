eb172bf4949eec9bd40a2ab25a71d14a84a5e881
SUHONG\topic <topic828@Naver.com>
2023-12-31 19:34:45 +0900

    우선순위큐

---
+++ Programmers/lv3/42628우선순위큐.js
@@ -0,0 +1,84 @@
+function solution(operations) {
+  var answer = [];
+  return answer;
+}
+
+function PriorityQueue(comparator) {
+  this._comparator = comparator;
+  this._elements = [];
+}
+
+PriorityQueue.prototype.isEmpty = function () {
+  return this.size() === 0;
+};
+
+PriorityQueue.prototype.peek = function () {
+  if (this.isEmpty()) throw new Error('PriorityQueue is empty');
+  return this._elements[0];
+};
+
+PriorityQueue.prototype.deq = function () {
+  let first = this.peek();
+  let last = this._elements.pop();
+  let size = this.size();
+
+  if (size === 0) return first;
+
+  this._elements[0] = last;
+  let current = 0;
+
+  while (current < size) {
+    let largest = current;
+    let left = 2 * current + 1;
+    let right = 2 * current + 2;
+
+    if (left < size && this._compare(left, largest) >= 0) {
+      largest = left;
+    }
+
+    if (right < size && this._compare(right, largest) >= 0) {
+      largest = right;
+    }
+
+    if (largest === current) break;
+
+    this._swap(largest, current);
+    current = largest;
+  }
+
+  return first;
+};
+
+PriorityQueue.prototype.enq = function (element) {
+  let size = this._elements.push(element);
+  let current = size - 1;
+
+  while (current > 0) {
+    let parent = Math.floor((current - 1) / 2);
+
+    if (this._compare(current, parent) <= 0) break;
+
+    this._swap(parent, current);
+    current = parent;
+  }
+
+  return size;
+};
+
+PriorityQueue.prototype.size = function () {
+  return this._elements.length;
+};
+
+PriorityQueue.prototype.forEach = function (fn) {
+  return this._elements.forEach(fn);
+};
+
+PriorityQueue.prototype._compare = function (a, b) {
+  return this._comparator(this._elements[a], this._elements[b]);
+};
+
+PriorityQueue.prototype._swap = function (a, b) {
+  let aux = this._elements[a];
+  this._elements[a] = this._elements[b];
+  this._elements[b] = aux;
+};
