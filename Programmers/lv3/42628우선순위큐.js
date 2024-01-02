01ade1b3b34a0b1e1471ec58dd560c56bbc7d418
SUHONG\topic <topic828@Naver.com>
2024-01-02 09:15:26 +0900

    힙

--- Programmers/lv3/42628우선순위큐.js
+++ Programmers/lv3/42628우선순위큐.js
@@ -1,100 +1,95 @@
-function PriorityQueue(comparator) {
-  this._comparator = comparator;
-  this._elements = [];
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  push(val) {
+    this.heap.push(val);
+    let currentIndex = this.heap.length - 1;
+    let parentIndex = Math.floor(currentIndex / 2);
+
+    while (
+      parentIndex !== 0 &&
+      this.heap[currentIndex] < this.heap[parentIndex]
+    ) {
+      this._swap(currentIndex, parentIndex);
+      currentIndex = parentIndex;
+      parentIndex = Math.floor(currentIndex / 2);
+    }
+  }
+
+  pop(isTopPop) {
+    if (this.isEmpty()) return;
+    if (this.heap.length === 2) return this.heap.pop();
+    if (!isTopPop) {
+      const parentIndex = Math.floor((this.heap.length - 1) / 2);
+      const lastLeaf = this.heap.slice(parentIndex);
+      const max = Math.max(...lastLeaf);
+      this._swap(parentIndex + lastLeaf.indexOf(max), this.heap.length - 1);
+      return this.heap.pop();
+    }
+
+    const val = this.heap[1];
+    this.heap[1] = this.heap.pop();
+
+    let currentIndex = 1;
+    let leftIndex = 2;
+    let rightIndex = 3;
+
+    while (
+      (this.heap[leftIndex] &&
+        this.heap[currentIndex] > this.heap[leftIndex]) ||
+      (this.heap[rightIndex] && this.heap[currentIndex] > this.heap[rightIndex])
+    ) {
+      if (this.heap[leftIndex] === undefined) {
+        this._swap(rightIndex, currentIndex);
+      } else if (this.heap[rightIndex] === undefined) {
+        this._swap(leftIndex, currentIndex);
+      } else if (this.heap[leftIndex] > this.heap[rightIndex]) {
+        this._swap(currentIndex, rightIndex);
+        currentIndex = rightIndex;
+      } else if (this.heap[leftIndex] <= this.heap[rightIndex]) {
+        this._swap(currentIndex, leftIndex);
+        currentIndex = leftIndex;
+      }
+
+      leftIndex = currentIndex * 2;
+      rightIndex = currentIndex * 2 + 1;
+    }
+
+    return val;
+  }
+
+  isEmpty() {
+    return this.heap.length === 1;
+  }
+
+  result() {
+    if (this.heap.length === 1) return [0, 0];
+    if (this.heap.length === 2) return [this.heap[1], this.heap[1]];
+    const parentIndex = Math.floor((this.heap.length - 1) / 2);
+    const lastLeaf = this.heap.slice(parentIndex);
+    const max = Math.max(...lastLeaf);
+    return [max, this.heap[1]];
+  }
+
+  _swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
 }
-
-PriorityQueue.prototype.isEmpty = function () {
-  return this.size() === 0;
-};
-
-PriorityQueue.prototype.peek = function () {
-  if (this.isEmpty()) throw new Error('PriorityQueue is empty');
-  return this._elements[0];
-};
-
-PriorityQueue.prototype.deq = function (value) {
-  let first = this.peek();
-  let last = this._elements.pop();
-  let size = this.size();
-
-  if (size === 0) return first;
-
-  this._elements[0] = last;
-  let current = 0;
-
-  while (current < size) {
-    let largest = current;
-    let left = 2 * current + 1;
-    let right = 2 * current + 2;
-
-    if (left < size && this._compare(left, largest) >= 0) {
-      largest = left;
-    }
-
-    if (right < size && this._compare(right, largest) >= 0) {
-      largest = right;
-    }
-
-    if (largest === current) break;
-
-    this._swap(largest, current);
-    current = largest;
-  }
-
-  return first;
-};
-
-PriorityQueue.prototype.enq = function (element) {
-  let size = this._elements.push(element);
-  let current = size - 1;
-
-  while (current > 0) {
-    let parent = Math.floor((current - 1) / 2);
-
-    if (this._compare(current, parent) <= 0) break;
-
-    this._swap(parent, current);
-    current = parent;
-  }
-
-  return size;
-};
-
-PriorityQueue.prototype.size = function () {
-  return this._elements.length;
-};
-
-PriorityQueue.prototype.forEach = function (fn) {
-  return this._elements.forEach(fn);
-};
-
-PriorityQueue.prototype._compare = function (a, b) {
-  return this._comparator(this._elements[a], this._elements[b]);
-};
-
-PriorityQueue.prototype._swap = function (a, b) {
-  let aux = this._elements[a];
-  this._elements[a] = this._elements[b];
-  this._elements[b] = aux;
-};
 
 function solution(operations) {
-  let queue = new PriorityQueue((a, b) => b[0] - a[0]);
-  operations.forEach((e) => {
-    const [operator, value] = e.split(' ');
-    if (operator === 'I') {
-      queue.enq(Number(value));
-    }else{
-        if(queue.isEmpty){
-            return
-        }
-        if(queue.)
+  const minHeap = new MinHeap();
+
+  operations.forEach((operation) => {
+    const [type, val] = operation
+      .split(' ')
+      .map((v, i) => (i === 1 ? Number(v) : v));
+    if (type === 'I') {
+      minHeap.push(val);
+    } else {
+      minHeap.pop(val < 0);
     }
   });
-  var answer = [];
-  return answer;
+  return minHeap.result();
 }
-
-console.log(
-  solution(['I 16', 'I -5643', 'D -1', 'D 1', 'D 1', 'I 123', 'D -1'])
-);
