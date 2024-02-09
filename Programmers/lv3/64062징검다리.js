52edfc2ae7a16a07398d8f7419e5f7f6a641ba70
SUHONG\topic <topic828@Naver.com>
2024-02-09 15:46:36 +0900

    징검다리

--- Programmers/lv3/64062징검다리.js
+++ Programmers/lv3/64062징검다리.js
@@ -1,39 +1,68 @@
+function solution(stones, k) {
+  // 이분탐색을 사용한다.
+  let left = 1;
+  let right = 200000000;
+
+  while (left <= right) {
+    const mid = ((left + right) / 2) >> 0;
+
+    let count = 0;
+    for (let i = 0; i < stones.length; i++) {
+      if (stones[i] - mid <= 0) count++;
+      else count = 0;
+
+      if (count === k) break;
+    }
+
+    if (count === k) right = mid - 1;
+    else left = mid + 1;
+  }
+
+  return left;
+}
+
+// 13번에서 걸림
 // function solution(stones, k) {
-//   let left = 1;
-//   let right = 200000000;
-//   while (left <= right) {
-//     const mid = ((left + right) / 2) >> 0;
-//     const copy = [...stones];
+//   const arrayLength = stones.length;
+//   if (arrayLength <= k) {
+//     return Math.max(...stones);
+//   }
+
+//   let answer = stones[0];
+
+//   for (let i = 1; i < k; i++) {
+//     if (stones[i] > answer) {
+//       answer = stones[i];
+//     }
+//   }
+
+//   for (let i = 0; i < arrayLength - k; i++) {
+//     let max = 0;
+//     const current = stones[i];
 //     let flag = false;
-//     let count = 0;
-
-//     for (let i = 0; i < copy.length; i++) copy[i] -= mid;
-
-//     for (const value of copy) {
-//       count = value <= 0 ? count + 1 : 0;
-
-//       if (count === k) {
+//     for (let j = 1; j <= k; j++) {
+//       if (current < stones[i + j]) {
 //         flag = true;
 //         break;
 //       }
+//       max = Math.max(max, stones[i + j]);
 //     }
-
-//     flag ? (right = mid - 1) : (left = mid + 1);
+//     if (!flag) {
+//       answer = Math.min(answer, max);
+//     }
 //   }
-
-//   return left;
+//   return answer;
 // }
 
 function solution(stones, k) {
-  //초기값
-  let answer = stones[0];
-  // 배열 길이 구해놓기
   const arrayLength = stones.length;
-  // 길이 보다 점프를
   if (arrayLength <= k) {
     return Math.max(...stones);
   }
-  for (let i = 0; i < stones.length - k; i++) {
+
+  let answer = stones[0];
+
+  for (let i = 0; i < arrayLength - k; i++) {
     let max = 0;
     const current = stones[i];
     let flag = false;
