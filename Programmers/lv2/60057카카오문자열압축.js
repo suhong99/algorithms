80dac90d1e3148c47be2b83c4fbbd3b0327c0af6
SUHONG\topic <topic828@Naver.com>
2024-02-15 06:26:32 +0900

    2번만틀림

--- Programmers/lv2/60057카카오문자열압축.js
+++ Programmers/lv2/60057카카오문자열압축.js
@@ -1,24 +1,29 @@
 function solution(s) {
   max = s.length;
   let answer = max;
-  for (let i = 1; i <= s / 2; i++) {
+  for (let i = 1; i <= max / 2; i++) {
     // 더는 할 필요 없을 경우 중지
-    let maxResult = parseInt(max / s) + (max % s);
+    const leftString = max % i;
+    let maxResult = parseInt(max / i) + leftString;
     if (maxResult > answer) break;
-    let currentResult = max % s;
+    // 나머지의 개수 만큼 미리 더하기
+    let currentResult = max % i;
     let prevValue;
-    let counted = false;
-    for (let j = 0; j < s / i; j += i) {
-      currentValue = s.subString(j, j + i);
+    let counted = 1;
+    let lastOne = max - leftString - i;
+
+    for (let j = 0; j < max - leftString; j += i) {
+      currentValue = s.substring(j, j + i);
       if (currentValue === prevValue) {
-        if (!counted) {
-          counted = true;
-          currentResult++;
-        }
+        counted++;
+        if (j === lastOne) currentResult += (counted + '').length;
         continue;
       }
       prevValue = currentValue;
-      counted = 0;
+      if (counted !== 1) {
+        currentResult += (counted + '').length;
+        counted = 1;
+      }
       currentResult += i;
     }
     answer = Math.min(answer, currentResult);
@@ -27,4 +32,5 @@ function solution(s) {
   return answer;
 }
 
-console.log(solution('aabbaccc'));
+// console.log(solution('aabbaccc'));
+console.log(solution('aaaaaaaaaaaabcd'));
