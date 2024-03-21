34b00f7795669723a1038668f75e4ed6ba86e4d7
SUHONG\topic <topic828@Naver.com>
2024-03-22 01:34:11 +0900

    위상정렬 줄세우기

---
+++ 100jun/gold/2252줄세우기.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+const input = fs
+  .readFileSync(__dirname + '/input.txt')
+  .toString()
+  .split('\n');
+
+const [n, m] = input[0].split(' ').map(Number);
+const graph = Array.from(Array(n + 1), () => []);
+
+const inDegrees = Array(n + 1).fill(0);
+
+for (let i = 1; i <= m; i++) {
+  const [small, big] = input[i].split(' ').map(Number);
+  graph[small].push(big);
+  inDegrees[big] += 1;
+}
+
+const queue = [];
+for (let i = 1; i <= n; i++) {
+  if (!inDegrees[i]) {
+    queue.push(i);
+  }
+}
+
+const result = [];
+while (queue.length) {
+  const i = queue.pop();
+  result.push(i);
+  graph[i].forEach((v) => {
+    inDegrees[v] -= 1;
+    if (!inDegrees[v]) {
+      queue.push(v);
+    }
+  });
+}
+
+console.log(result.join(' '));
