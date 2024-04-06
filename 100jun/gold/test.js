12d268b1e20cbee595da0c47dde72cd59701f9c0
SUHONG\topic <topic828@Naver.com>
2024-04-06 10:27:08 +0900

    연습

--- 100jun/gold/test.js
+++ 100jun/gold/test.js
@@ -1,19 +1,48 @@
-const arr = [1, 2, 3, 4, 5, 6, 10];
-// arr.forEach((element) => console.log(element));
+// // const arr = [1, 2, 3, 4, 5, 6, 10];
+// // // arr.forEach((element) => console.log(element));
 
-// arr.forEach((element, index, array) => console.log(element, index, array));
+// // // arr.forEach((element, index, array) => console.log(element, index, array));
 
-// const mapArr = arr.map(function (element) {
-//   console.log(element);
-//   return element * 2;
-// });
+// // // const mapArr = arr.map(function (element) {
+// // //   console.log(element);
+// // //   return element * 2;
+// // // });
 
-// const mapArr2 = arr.map((e, i, a) => {
-//   console.log(e, i, a);
-//   return e * 3;
-// });
+// // // const mapArr2 = arr.map((e, i, a) => {
+// // //   console.log(e, i, a);
+// // //   return e * 3;
+// // // });
 
-const str = 'hello world';
-console.log(str.split(' '));
+// // const str = 'hello world';
+// // console.log(str.split(' '));
 
-console.log(str.split(' ').join('+'));
+// // console.log(str.split(' ').join('+'));
+
+// // array.forEach(element => {
+
+// // });
+
+// const array = [
+//   '/',
+//   '/hello',
+//   '/hello/tmp',
+//   '/root',
+//   '/root/abcd',
+//   '/root/abcd/etc',
+//   '/root/abcd/hello',
+//   '/root/aa',
+// ];
+
+// let arrayB = array.sort((a, b) => a.localeCompare(b));
+
+// console.log(array, arrayB);
+
+// const i = 0; //좌
+// const r = 10; //우
+// const b = 0;
+// const t = 10;
+// const x = 92;
+// const y = 83;
+// console.log(i + (x % (r - i)), b + (y % (t - b)));
+
+console.log(3 ** 2);
