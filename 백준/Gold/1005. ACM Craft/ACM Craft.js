8e800f7c0a9ca59a63f73cce305416ba530a5dec
suhong kim <120103909+suhong99@users.noreply.github.com>
2024-04-13 10:29:05 +0900

    [Gold III] Title: ACM Craft, Time: 1252 ms, Memory: 109968 KB -BaekjoonHub

---
+++ 백준/Gold/1005. ACM Craft/ACM Craft.js
@@ -0,0 +1,40 @@
+const fs = require('fs');
+const input = fs
+  .readFileSync('dev/stdin')
+  .toString()
+  .split('\n');
+
+const T = +input[0];
+let index = 1;
+
+for (let t = 1; t <= T; t++) {
+  const [n, k] = input[index].split(' ').map(Number);
+  const timeArr = input[index + 1].split(' ').map(Number);
+  index += 2;
+  const graph = Array(n + 1)
+    .fill()
+    .map(() => []);
+  const minBuild = [0, ...timeArr];
+  for (let i = 0; i < k; i++) {
+    const [start, end] = input[index + i].split(' ').map(Number);
+    graph[end].push(start);
+  }
+
+  index += k;
+  const target = input[index++];
+  countTotal(target);
+  console.log(minBuild[target]);
+
+  function countTotal(number) {
+    while (true) {
+      if (graph[number].length === 0) {
+        return minBuild[number];
+      }
+      const subTarget = graph[number].pop();
+      minBuild[number] = Math.max(
+        minBuild[number],
+        countTotal(subTarget) + timeArr[number - 1]
+      );
+    }
+  }
+}
