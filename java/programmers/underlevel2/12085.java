f1bd09196b64c8abc2fe3e7ea2d78bd00319e1e4
SUHONG\topic <topic828@Naver.com>
2024-07-21 17:00:58 +0900

    java풀기

---
+++ java/programmers/underlevel2/12085.java
@@ -0,0 +1,7 @@
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805?language=java
+
+class Solution {
+    public int solution(int num1, int num2) {
+     return num1/num2;
+    }       
+}
--- 기타/프리온보딩5월.js
+++ 기타/프리온보딩5월.js
@@ -1,11 +1,16 @@
-function printAdultStatue(age) {
-  age >= 18 && console.log('어른임');
-  // 올바른 사용은 문을 쓰는거다
-}
+// function printAdultStatue(age) {
+//   age >= 18 && console.log('어른임');
+//   // 올바른 사용은 문을 쓰는거다
+// }
 
-const b = console.log('t');
-console.log(b);
+// const b = console.log('t');
+// console.log(b);
 
 //식 : 값을 생성하여 반환함
 // 문 : 값이 반환될 필요가 없음
-printAdultStatue(20);
+// printAdultStatue(20);
+
+const today = new Date();
+const targetDay = new Date('2025-12-31');
+
+console.log(today);
