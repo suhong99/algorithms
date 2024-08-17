7d432b7b476166f59b3d7feae917803429dce991
SUHONG\topic <topic828@Naver.com>
2024-08-17 19:18:23 +0900

    점찍기

--- 프로그래머스/2/note.js
+++ 프로그래머스/2/note.js
@@ -2,6 +2,12 @@ function solution(k, d) {
   let answer = 0;
   const maxSqrt = d ** 2;
   for (let i = 0; i <= d; i += k) {
+    console.log(
+      i,
+      answer,
+      'Add : ',
+      Math.floor(Math.sqrt(maxSqrt - i ** 2) / k) + 1
+    );
     answer += Math.floor(Math.sqrt(maxSqrt - i ** 2) / k) + 1;
   }
   return answer;
