91d01802e9ec92af6c1933c466d2940256c4c551
SUHONG\topic <topic828@Naver.com>
2024-08-22 13:20:32 +0900

    피보나치

---
+++ Programmers/lv2/피보나치수.js
@@ -0,0 +1,9 @@
+function solution(n) {
+  const pibonachi = [0, 1, 1, 2, 3, 5, 8, 13];
+  for (let i = 7; i < n; i++) {
+    pibonachi.push((pibonachi[i - 1] + pibonachi[i]) % 1234567);
+  }
+  return pibonachi[n];
+}
+
+console.log(solution(5));
