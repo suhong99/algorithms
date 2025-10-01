5f832b641285eca417d4311761c61d1855781dbb
suhong99 <topic828@naver.com>
2025-10-01 22:50:30 +0900

    카카오문제 복습

--- Programmers/lv1/가장많이받은선물.js
+++ Programmers/lv1/가장많이받은선물.js
@@ -1,41 +1,44 @@
 // https://school.programmers.co.kr/learn/courses/30/lessons/258712
 
 function solution(friends, gifts) {
-  const nameToIndex = friends.reduce((obj, name, index) => {
-    obj[name] = index;
-    return obj;
-  }, {});
+	// 친구 명단을 값이 index인 객체로 변경
+	const nameToIndex = friends.reduce((obj, name, index) => {
+		obj[name] = index;
+		return obj;
+	}, {});
 
-  const matrix = Array(friends.length)
-    .fill()
-    .map(() => Array(friends.length).fill(0));
+	const matrix = Array(friends.length)
+		.fill()
+		.map(() => Array(friends.length).fill(0));
 
-  const giftPoint = Array(friends.length).fill(0);
-  const gift = Array(friends.length).fill(0);
-  console.log(matrix, 'matrix');
-  for (let i = 0; i < gifts.length; i++) {
-    const [from, to] = gifts[i].split(' ').map((name) => nameToIndex[name]);
-    matrix[from][to] -= 1;
-    matrix[to][from] += 1;
-    giftPoint[from] -= 1;
-    giftPoint[to] += 1;
-  }
+	const giftPoint = Array(friends.length).fill(0);
+	const gift = Array(friends.length).fill(0);
 
-  for (let i = 0; i < friends.length - 1; i++) {
-    for (let j = i + 1; j < friends.length; j++) {
-      if (matrix[i][j] > 0) {
-        gift[j] += 1;
-      } else if (matrix[i][j] < 0) {
-        gift[i] += 1;
-      } else if (giftPoint[i] > giftPoint[j]) {
-        gift[j] += 1;
-      } else if (giftPoint[i] < giftPoint[j]) {
-        gift[i] += 1;
-      }
-    }
-  }
+	// 선물 거래 내역 반영
+	for (let i = 0; i < gifts.length; i++) {
+		const [from, to] = gifts[i].split(' ').map((name) => nameToIndex[name]);
+		matrix[from][to] -= 1;
+		matrix[to][from] += 1;
+		giftPoint[from] -= 1;
+		giftPoint[to] += 1;
+	}
 
-  return Math.max(...gift);
+	// j의 인덱스를 i보다 크게하여 중복되게 선물을 더하지 않게 함
+	for (let i = 0; i < friends.length - 1; i++) {
+		for (let j = i + 1; j < friends.length; j++) {
+			if (matrix[i][j] > 0) {
+				gift[j] += 1;
+			} else if (matrix[i][j] < 0) {
+				gift[i] += 1;
+			} else if (giftPoint[i] > giftPoint[j]) {
+				gift[j] += 1;
+			} else if (giftPoint[i] < giftPoint[j]) {
+				gift[i] += 1;
+			}
+		}
+	}
+
+	return Math.max(...gift);
 }
 
 // 아래 코드가 더 빠름.
@@ -43,33 +46,33 @@ function solution(friends, gifts) {
 // 사실 줬다는 사실만 기록해도 충분하고, 대칭되는 열을 비교하는게 빠름
 
 function solution(friends, gifts) {
-  const length = friends.length;
-  const giftPoints = new Array(length).fill(0);
-  const index = {};
-  const record = [];
-  const points = new Array(length).fill(0);
-  for (let i = 0; i < length; i++) {
-    record[i] = new Array(length).fill(0);
-    index[friends[i]] = i;
-  }
-  for (const gift of gifts) {
-    const [giver, taker] = gift.split(' ');
-    record[index[giver]][index[taker]] += 1;
-    giftPoints[index[giver]] += 1;
-    giftPoints[index[taker]] -= 1;
-  }
-  for (let i = 0; i < length; i++) {
-    for (let j = 0; j < length; j++) {
-      if (record[i][j] > record[j][i]) {
-        points[i] += 1;
-      } else if (record[i][j] === record[j][i]) {
-        if (giftPoints[i] > giftPoints[j]) {
-          points[i] += 1;
-        }
-      }
-    }
-  }
-  return Math.max(...points);
+	const length = friends.length;
+	const giftPoints = new Array(length).fill(0);
+	const index = {};
+	const record = [];
+	const points = new Array(length).fill(0);
+	for (let i = 0; i < length; i++) {
+		record[i] = new Array(length).fill(0);
+		index[friends[i]] = i;
+	}
+	for (const gift of gifts) {
+		const [giver, taker] = gift.split(' ');
+		record[index[giver]][index[taker]] += 1;
+		giftPoints[index[giver]] += 1;
+		giftPoints[index[taker]] -= 1;
+	}
+	for (let i = 0; i < length; i++) {
+		for (let j = 0; j < length; j++) {
+			if (record[i][j] > record[j][i]) {
+				points[i] += 1;
+			} else if (record[i][j] === record[j][i]) {
+				if (giftPoints[i] > giftPoints[j]) {
+					points[i] += 1;
+				}
+			}
+		}
+	}
+	return Math.max(...points);
 }
 
 solution(['a', 'b', 'c'], ['a b', 'b a', 'c a', 'a c', 'a c', 'c a']);
--- Programmers/lv2/도넛과막대그래프.js
+++ Programmers/lv2/도넛과막대그래프.js
@@ -14,105 +14,109 @@
 //35 번 런타임에러
 
 function solution(edges) {
-  let point;
-  let stick = 0,
-    oct = 0;
+	let point;
+	let stick = 0,
+		oct = 0;
 
-  const graphOut = [];
-  const graphIn = [];
-  edges.forEach((element) => {
-    const [start, end] = element;
-    insert(start, end, graphOut);
-    insert(end, start, graphIn);
-  });
+	const graphOut = [];
+	const graphIn = [];
+	edges.forEach((element) => {
+		const [start, end] = element;
+		insert(start, end, graphOut);
+		insert(end, start, graphIn);
+	});
 
-  const max = Math.max(graphIn.length, graphOut.length);
+	const max = Math.max(graphIn.length, graphOut.length);
 
-  for (let i = 1; i < graphIn.length; i++) {
-    if (
-      graphIn[i] === undefined &&
-      graphOut[i] !== undefined &&
-      graphOut[i].length > 1
-    ) {
-      point = i;
-      break;
-    }
-  }
+	for (let i = 1; i < graphIn.length; i++) {
+		if (graphIn[i] === undefined && graphOut[i] !== undefined && graphOut[i].length > 1) {
+			point = i;
+			break;
+		}
+	}
 
-  const connected = graphOut[point];
-  connected.forEach((element) => {
-    graphIn[element] = graphIn[element].filter((e) => e !== point);
-  });
+	// 새로 생긴 정점에서 나오는 간선이 연결된 도형 정보
+	const connected = graphOut[point];
 
-  let donut = connected.length;
+	// 새로 생긴 정점 관련 간선을 제거
+	connected.forEach((element) => {
+		graphIn[element] = graphIn[element].filter((e) => e !== point);
+	});
 
-  for (let i = 1; i < max; i++) {
-    if (i === point) continue;
-    if (donut === 0) break;
+	// 정점에서 나온 간선의 수가 총 도형의 수 (일단 모두 도넛으로 고려함)
+	let donut = connected.length;
 
-    if (graphIn[i] === undefined && graphOut[i] !== undefined) {
-      stick++;
-      donut--;
-      continue;
-    }
+	for (let i = 1; i < max; i++) {
+		if (i === point) continue;
+		if (donut === 0) break;
 
-    if (graphIn[i] !== undefined && graphIn[i].length === 0) {
-      stick++;
-      donut--;
-      continue;
-    }
-    if (graphOut[i] && graphOut[i].length === 2) {
-      oct++;
-      donut--;
-    }
-  }
+		// 막대의 시작점은 들어오는건 없고 나가는 건 ㅇ닛음
+		if (graphIn[i] === undefined && graphOut[i] !== undefined) {
+			stick++;
+			donut--;
+			continue;
+		}
 
-  return [point, donut, stick, oct];
+		// size가 1인 막대
+		if (graphIn[i] !== undefined && graphIn[i].length === 0) {
+			stick++;
+			donut--;
+			continue;
+		}
+
+		// 도넛의 가운데 지점만 나가는게 2개임
+		if (graphOut[i] && graphOut[i].length === 2) {
+			oct++;
+			donut--;
+		}
+	}
+
+	return [point, donut, stick, oct];
 }
 
 function insert(i, val, arr) {
-  if (arr[i] === undefined) {
-    arr[i] = [val];
-  } else {
-    arr[i].push(val);
-  }
+	if (arr[i] === undefined) {
+		arr[i] = [val];
+	} else {
+		arr[i].push(val);
+	}
 }
 
 console.log(
-  solution([
-    [1, 12],
-    [8, 3],
-    [12, 7],
-    [7, 11],
-    [9, 6],
-    [10, 11],
-    [6, 10],
-    [3, 5],
-    [11, 1],
-    [5, 3],
-    [11, 9],
-    [3, 8],
-    [4, 11],
-    [4, 8],
-  ])
+	solution([
+		[1, 12],
+		[8, 3],
+		[12, 7],
+		[7, 11],
+		[9, 6],
+		[10, 11],
+		[6, 10],
+		[3, 5],
+		[11, 1],
+		[5, 3],
+		[11, 9],
+		[3, 8],
+		[4, 11],
+		[4, 8],
+	]),
 );
 
 console.log(
-  solution([
-    [4, 11],
-    [1, 12],
-    [8, 3],
-    [12, 7],
-    [4, 2],
-    [7, 11],
-    [4, 8],
-    [9, 6],
-    [10, 11],
-    [6, 10],
-    [3, 5],
-    [11, 1],
-    [5, 3],
-    [11, 9],
-    [3, 8],
-  ])
+	solution([
+		[4, 11],
+		[1, 12],
+		[8, 3],
+		[12, 7],
+		[4, 2],
+		[7, 11],
+		[4, 8],
+		[9, 6],
+		[10, 11],
+		[6, 10],
+		[3, 5],
+		[11, 1],
+		[5, 3],
+		[11, 9],
+		[3, 8],
+	]),
 );
